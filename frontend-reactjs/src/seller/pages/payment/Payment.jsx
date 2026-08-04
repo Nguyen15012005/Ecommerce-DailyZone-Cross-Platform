2d8ce1138d4f18ac40a5e3cc60a53d1f36086c73
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-08-04 16:53:09 +0700

    commit seller function

--- frontend-reactjs/src/seller/pages/payment/Payment.jsx
+++ frontend-reactjs/src/seller/pages/payment/Payment.jsx
@@ -1,9 +1,19 @@
+import { Card, Divider } from "@mui/material";
 import React from "react";
 
 const Payment = () => {
   return (
     <div>
-      <h1>Payment</h1>
+      <h1 className="">
+        <Card className="rounded-md space-y-4 p-5">
+          <h1 className="text-gray-600 font-medium">Tổng thu nhập</h1>
+          <h1 className="font-bold text-xl pb-1">9876</h1>
+          <Divider />
+          <p className="text-gray-600 font-medium pt-1">
+            Phương thức thanh toán gần nhất: <strong>0đ</strong>
+          </p>
+        </Card>
+      </h1>
     </div>
   );
 };
