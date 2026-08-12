0a77b03232877c5985cf06f3792a6c9eebc8d00d
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-08-12 23:44:22 +0700

    update code

--- frontend-reactjs/src/routes/AppRoutes.jsx
+++ frontend-reactjs/src/routes/AppRoutes.jsx
@@ -1,20 +1,20 @@
-// import React from "react";
-// import { useRoutes } from "react-router-dom";
-// import { customerRoutes } from "./CustomerRoutes";
-// import { sellerRoutes } from "./SellerRoutes";
-// import ScrollToTop from "./ScrollToTop";
-// import { adminRoutes } from "./AdminRoutes";
+import React from "react";
+import { useRoutes } from "react-router-dom";
+import { customerRoutes } from "./CustomerRoutes";
+import { sellerRoutes } from "./SellerRoutes";
+import ScrollToTop from "./ScrollToTop";
+import { adminRoutes } from "./AdminRoutes";
 
-// const AppRoutes = () => {
-//   const routes = [...customerRoutes, ...sellerRoutes, ...adminRoutes];
-//   const routing = useRoutes(routes);
+const AppRoutes = () => {
+  const routes = [...customerRoutes, ...sellerRoutes, ...adminRoutes];
+  const routing = useRoutes(routes);
 
-//   return (
-//     <>
-//       <ScrollToTop />
-//       {routing}
-//     </>
-//   );
-// };
+  return (
+    <>
+      <ScrollToTop />
+      {routing}
+    </>
+  );
+};
 
-// export default AppRoutes;
+export default AppRoutes;
