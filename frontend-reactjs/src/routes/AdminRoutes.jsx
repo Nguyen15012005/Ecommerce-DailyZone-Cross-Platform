f8d6aef5e8d770f18f10bc85453183aa617703e5
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-08-12 20:26:21 +0700

    update code admin

--- frontend-reactjs/src/routes/AdminRoutes.jsx
+++ frontend-reactjs/src/routes/AdminRoutes.jsx
@@ -2,6 +2,7 @@ import AdminHome from "../admin/pages/home/AdminHome";
 import ProtectedRoute from "./ProtectedRoute";
 import AdminLogin from "../admin/pages/auth/LoginAdmin";
 import Dashboard from "../admin/pages/dashboard/Dashboard";
+import Coupon from "./../admin/pages/coupon/Coupon";
 
 export const adminRoutes = [
   {
@@ -12,13 +13,21 @@ export const adminRoutes = [
   {
     path: "/admin",
     element: (
-      <ProtectedRoute allowedRoles={["ADMIN"]}>
-        <AdminHome />
-      </ProtectedRoute>
+      // <ProtectedRoute allowedRoles={["ADMIN"]}>
+      <AdminHome />
+      // </ProtectedRoute>
     ),
     children: [
       {
         index: true,
+        element: <Dashboard />,
+      },
+      {
+        path: "",
+        element: <Coupon />,
+      },
+      {
+        path: "",
         element: <Dashboard />,
       },
     ],
