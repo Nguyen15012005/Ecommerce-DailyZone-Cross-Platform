35d8f5b7e1bae7cd8ade2d6c3500fa9383ecd916
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-08-19 23:48:28 +0700

    update login admin

--- frontend-reactjs/src/admin/pages/auth/LoginAdmin.jsx
+++ frontend-reactjs/src/admin/pages/auth/LoginAdmin.jsx
@@ -4,10 +4,10 @@ import { loginWithOtp, sendAdminLoginOtp } from "../../../store/authSlice";
 export default function AdminLogin() {
   return (
     <AuthOtpLogin
-      sendOtpAction={sendAdminLoginOtp}
-      loginAction={loginWithOtp}
-      title="Đăng Nhập Quản Trị Hệ Thống"
-      submitLabel="Đăng nhập"
+    // sendOtpAction={sendAdminLoginOtp}
+    // loginAction={loginWithOtp}
+    // title="Đăng Nhập Quản Trị Hệ Thống"
+    // submitLabel="Đăng nhập"
     />
   );
 }
